920c068ca60c1d7ce8f19ac5f24ecb57b372850d
Oliver P <GitHub@opott.is-cool.me>
2024-10-27 09:28:35 +0000

    upload pset_0 indoor

---
+++ psets/pset_0/indoor/indoor.py
@@ -0,0 +1,8 @@
+# get the user's input and store it in the content variable
+content = input()
+
+# reassign the content variable to the existing value, but in lower case
+content = content.lower()
+
+# print the value of the content variable
+print(content)
