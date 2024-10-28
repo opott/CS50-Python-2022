48f0bbbe22450e1bf3d9e43ea07f0cd46699d931
Oliver P <GitHub@opott.is-cool.me>
2024-10-28 09:44:55 +0000

    upload pset_0 playback

---
+++ psets/pset_0/playback/playback.py
@@ -0,0 +1,8 @@
+# get the users input and store it in the content variable
+content = input()
+
+# replace the whitespace in the content variable with '...' and store it in the output variable
+output = content.replace(' ', '...')
+
+# print the value of the output variable
+print(output)
