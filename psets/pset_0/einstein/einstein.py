01d76be23cb38a7e8ceadeb7af3fb8b4331e06de
Oliver P <GitHub@opott.is-cool.me>
2024-10-28 10:02:14 +0000

    upload pset_0 einstein

---
+++ psets/pset_0/einstein/einstein.py
@@ -0,0 +1,11 @@
+# get the value of m from the user
+m = int(input("m = "))
+
+# define the value of c
+c = 300000000
+
+# define the value of E as mc^2
+E = (c*c) * m
+
+# print the value of E
+print(f"E = {E}")
