180fe1378e132893b7019947ed1958a26f5fda29
Oliver P <120502505+opott@users.noreply.github.com>
2024-10-29 15:14:35 +0000

    upload pset_1 extensions

---
+++ psets/pset_1/extensions/extensions.py
@@ -0,0 +1,20 @@
+# get input from the user
+filename = input("File Name: ")
+
+# convert file name to lower case & strip whitespace
+filename = filename.lower().strip()
+
+if filename.endswith('.gif'):
+    print("image/gif")
+elif filename.endswith('.jpg') or filename.endswith('.jpeg'):
+    print("image/jpeg")
+elif filename.endswith('.png'):
+    print("image/png")
+elif filename.endswith('.pdf'):
+    print("application/pdf")
+elif filename.endswith('.txt'):
+    print("text/plain")
+elif filename.endswith('.zip'):
+    print("application/zip")
+else:
+    print("application/octet-stream")
